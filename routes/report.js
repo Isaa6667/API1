232209f3656b67cdafb0ae96e161087116cc3cf8
Isaa6667 <89317987+Isaa6667@users.noreply.github.com>
2026-04-14 16:59:40 +0100

    Replace POST with GET for report retrieval

--- routes/report.js
+++ routes/report.js
@@ -2,10 +2,13 @@ const express = require("express")
 const router = express.Router()
 const store = require("../store")
 
-router.post("/", (req, res) => {
-    const data = req.body
-    store.reports[data.id] = data
-    res.json({ ok: true })
+router.get("/", (req, res) => {
+    const keys = Object.keys(store.reports)
+    if (keys.length === 0) return res.json({})
+    const id = keys[0]
+    const report = store.reports[id]
+    delete store.reports[id]
+    res.json(report)
 })
 
 module.exports = router
